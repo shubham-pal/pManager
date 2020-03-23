7c7fd416d7913061a626f7f4ae69a6df2982ad8d
shubham-pal <shubham.mca19.du@gmail.com>
2020-03-23 13:23:05 +0530

    sidebar menu

--- app/Company.php
+++ app/Company.php
@@ -15,4 +15,8 @@ class Company extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function projects() {
+        return $this->hasMany('App\Project');
+    }
 }
--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -2,27 +2,45 @@
 
 @section('content')
 
-<div class="jumbotron">
-    <div class="container">
-      <h1 class="display-3">{{ $company->name }}</h1>
-      <p>{{ $company->description }}</p>
-      <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p> -->
+<div class="col-md-9 col-lg-9 pull-left">
+    <div class="jumbotron">
+        <div class="container">
+        <h1 class="display-3">{{ $company->name }}</h1>
+        <p>{{ $company->description }}</p>
+        <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p> -->
+        </div>
+    </div>
+
+    <div class="row" style="background: white; margin: 10px;">
+        @foreach($comapny->projects as $project)
+        <div class="col-md-4">
+            <h2>{{ $project->name }}</h2>
+            <p>{{ $project->description }}</p>
+            <p><a class="btn btn-secondary" href="/projects/{{ $project->id }}" role="button">View Project details &raquo;</a></p>
+        </div>
+        @endforeach
     </div>
 </div>
-<div class="col-md-4">
-    <h2>Heading</h2>
-    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-</div>
-<div class="col-md-4">
-    <h2>Heading</h2>
-    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-</div>
-<div class="col-md-4">
-    <h2>Heading</h2>
-    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+
+<div class="col-md-3 col-lg-3 pull-right">
+    <!-- <div class="sidebar-module sidebar-module-inset">
+        <h4></h4>
+        <p></p>
+    </div> -->
+    <div class="sidebar-module">
+        <h4>Actions</h4>
+        <ol class="list-unstyled">
+            <li><a href="#">Edit</a></li>
+            <li><a href="#">Delete</a></li>
+            <li><a href="#">Add new member</a></li>
+        </ol>
+    </div>
+    <!-- <div class="sidebar-module">
+        <h4>Members</h4>
+        <ol class="list-unstyled">
+            <li><a href="#"></a></li>
+        </ol>
+    </div> -->
 </div>
 
 @endsection
